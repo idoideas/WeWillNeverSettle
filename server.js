1f2f6280a97a7c88f184b5b75fbfa621ea2d3708
Idoideas <idoideasmail@gmail.com>
2018-04-28 00:56:03 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var url = require("url");
+fs = require('fs');
+
+var number = 0;
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.use('/', express.static(__dirname + '/'));
+
+io.on('connection', function(socket){
+    socket.on('add-number', function(msg){
+        number++;
+        io.emit('add-number', number);
+    });
+});
+
+http.listen(8080, function(){
+    console.log('listening on *:8080');
+});
