89b43237b2ba6285b80488c79c696755e0af5585
Idoideas <idoideasmail@gmail.com>
2018-04-28 04:19:00 +0300

    Added request for adding number

--- server.js
+++ server.js
@@ -4,12 +4,25 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var url = require("url");
 fs = require('fs');
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );
 
 var number = 0;
 var isMuted = false;
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/counterRequest', function(req, res){
+    if(req.headers["seed"]==="confirmed"){
+        number++;
+        io.emit('add-number', number);
+        res.send("Thanks for never settling!")
+    } else {
+        res.send("I appericate the try. However, this project doesn't support non-clean requests. Please don't do that.")
+    }
+
 });
 
 app.use('/', express.static(__dirname + '/'));
